47b2647158d44e2477851957a32e5243c59970ac
Jay Prall <jay.prall@affectiva.com>
2013-11-17 18:09:44 -0500

    initial import of grails-red5-0.1.zip from http://cantina.co/2007/11/20/red5-plugin-for-grails/

---
+++ grails-app/controllers/DemoController.groovy
@@ -0,0 +1,4 @@
+class DemoController {
+
+    def index = { }
+}
---
+++ src/java/com/cantinaconsulting/grails/artifact/handler/Red5AppArtefactHandler.java
@@ -0,0 +1,43 @@
+/* Copyright 2006-2007 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* For more information please visit www.cantinaconsulting.com
+* or email info@cantinaconsulting.com
+*/
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mchisholm
+ * Date: Nov 9, 2007
+ * To change this template use File | Settings | File Templates.
+ */
+
+package com.cantinaconsulting.grails.artifact.handler;
+
+import org.codehaus.groovy.grails.commons.ArtefactHandlerAdapter;
+
+public class Red5AppArtefactHandler extends ArtefactHandlerAdapter {
+
+    public static final String TYPE = "Red5App";
+
+
+    public Red5AppArtefactHandler() {
+        super(TYPE, GrailsRed5AppClass.class, DefaultGrailsRed5AppClass.class, null);
+    }
+
+    public boolean isArtefactClass( Class clazz ) {
+        if(clazz == null) return false;
+        return clazz.getName().endsWith(DefaultGrailsRed5AppClass.RED5APP);
+    }
+}
